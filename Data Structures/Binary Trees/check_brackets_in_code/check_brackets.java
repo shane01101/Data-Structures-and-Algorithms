61311c74b5806f65eeeff10063f5bc3d83d1a815
Shane Honanie <shaneh425@gmail.com>
2018-03-29 14:51:34 -0700

    Binary Tree Updated

--- Data Structures/Binary Trees/check_brackets_in_code/check_brackets.java
+++ Data Structures/Binary Trees/check_brackets_in_code/check_brackets.java
@@ -23,47 +23,52 @@ class Bracket {
     int position;
 }
 
-class check_brackets {
-    public static void main(String[] args) throws IOException {
+class check_brackets 
+{
+    public static void main(String[] args) throws IOException 
+    {
         InputStreamReader input_stream = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input_stream);
         String text = reader.readLine();
-        int invalidBracket = -1;
+        int invalidIndex = -1;
 
         Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
-        for (int position = 0; position < text.length(); ++position) {
+        for (int position = 0; position < text.length(); ++position) 
+        {
             char next = text.charAt(position);
 
-            if (next == '(' || next == '[' || next == '{') {
-                opening_brackets_stack.push(new Bracket(next, position));
+            if (next == '(' || next == '[' || next == '{') 
+            {
+                opening_brackets_stack.push(new Bracket(text.charAt(position), position));
             }
-            else if (next == ')' || next == ']' || next == '}') {
+            
+            if (next == ')' || next == ']' || next == '}') 
+            {
                 if(!opening_brackets_stack.isEmpty())
                 {
                     Bracket top = opening_brackets_stack.pop();
+                    
                     if(!top.Match(next))
                     {
-                        invalidBracket = position + 1;
+                        invalidIndex = position + 1;
                         break;
                     }
                 }
-                else
+                else //empty
                 {
-                    invalidBracket = position + 1;
+                    invalidIndex = position + 1;
                     break;
                 }
             }
         }
-        
-        if(!opening_brackets_stack.isEmpty() && invalidBracket == -1) 
-        {
-            invalidBracket = opening_brackets_stack.get(0).position + 1;
-        }
+        //invalid index must still be in the stack if no break occurred
+        if(!opening_brackets_stack.isEmpty() && invalidIndex == -1)
+            invalidIndex = opening_brackets_stack.pop().position + 1;
 
-        // Printing answer, write your code here
-        if(invalidBracket == -1)
-            System.out.println("Success");
+        //Print Answer
+        if(invalidIndex != -1)
+            System.out.println(invalidIndex);
         else
-           System.out.println(invalidBracket); 
+            System.out.println("Success");
     }
 }
--- Data Structures/Binary Trees/tree_height/tree_height.java
+++ Data Structures/Binary Trees/tree_height/tree_height.java
@@ -1,107 +1,115 @@
 import java.util.*;
 import java.io.*;
 
-public class tree_height {
-    class FastScanner {
-		StringTokenizer tok = new StringTokenizer("");
-		BufferedReader in;
+public class tree_height 
+{
+    class FastScanner 
+    {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
 
-		FastScanner() {
-			in = new BufferedReader(new InputStreamReader(System.in));
-		}
+        FastScanner() 
+        {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		String next() throws IOException {
-			while (!tok.hasMoreElements())
-				tok = new StringTokenizer(in.readLine());
-			return tok.nextToken();
-		}
-		int nextInt() throws IOException {
-			return Integer.parseInt(next());
-		}
-	}
+        String next() throws IOException 
+        {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+        int nextInt() throws IOException 
+        {
+            return Integer.parseInt(next());
+        }
+    }
 
-	public class TreeHeight {
-		int n;
-		int parent[];
-		
-		void read() throws IOException {
-			FastScanner in = new FastScanner();
-			n = in.nextInt();
-			parent = new int[n];
-			for (int i = 0; i < n; i++) {
-				parent[i] = in.nextInt();
-			}
-		}
+    public class TreeHeight 
+    {
+        int n;
+        int parent[];
 
-		int computeHeight() 
-                {
-                    // Replace this code with a faster implementation
-                    int maxHeight = 0;
-                    for (int vertex = 0; vertex < n; vertex++) {
-                            int height = 0;
-                            for (int i = vertex; i != -1; i = parent[i])
-                                    height++;
-                            maxHeight = Math.max(maxHeight, height);
-                    }
-                    return maxHeight;
-		}
+        void read() throws IOException 
+        {
+            FastScanner in = new FastScanner();
+            n = in.nextInt();
+            parent = new int[n];
+            for (int i = 0; i < n; i++) 
+            {
+                parent[i] = in.nextInt();
+            }
+        }
+
+        int computeHeight() 
+        {
+            // Replace this code with a faster implementation
+            int maxHeight = 0;
+            for (int vertex = 0; vertex < n; vertex++) 
+            {
+                int height = 0;
+                for (int i = vertex; i != -1; i = parent[i])
+                    height++;
+                maxHeight = Math.max(maxHeight, height);
+            }
+            return maxHeight;
+        }
+        
+        int computEfficientHeight() 
+        {
+            int[] nodeDepth = new int[n];
+            int height = 0;
+            
+            if(n==1 && parent[0] == -1)
+                return 1;
+            
+            for(int i = 0; i < nodeDepth.length; i++)
+            {
+                height = Math.max(height, computEfficientHeightHelper(parent, nodeDepth, i));
+            }
                 
-                int computeHeightOptimal() 
-                {
-                    int[] nodeDepth = new int[n];
-                    int height = 0;
-                    
-                    if(n == 1 && parent[0] == -1)
-                        return 1;
-                    
-                    //compute depths for each node
-                    for(int i = 0; i < nodeDepth.length; i++)
-                    {
-                        height = Math.max(height, computeHeightOptimalHelper(
-                                                        parent, nodeDepth, i));
-                    }
-                    return height;
-		}
-                
-                int computeHeightOptimalHelper(int[] parent, int[] depths, 
-                                                    int index) 
-                {
-                    //already computed so return
-                    if (depths[index] != 0)
-                        return depths[index];
-                    
-                    if (parent[index] == -1) //Root node
-                    {
-                        depths[index] = 1;
-                        return 1;
-                    }
-                    //Calculate the parent depth if not done yet
-                    if (depths[parent[index]] == 0) 
-                    {
-                        computeHeightOptimalHelper(parent, depths, 
-                                                        parent[index]);
-                    }   
-                    
-                    //depth is parent depth + 1
-                    depths[index] = depths[parent[index]] + 1;
-                    return depths[index];
-                }
-	}
+            return height;
+        }
+        
+        int computEfficientHeightHelper(int[] parent, int[] depths, int index)
+        {
+            //already computed
+            if(depths[index] > 0)
+                return depths[index];
+            //root
+            if(parent[index] == -1)
+            {
+                depths[index] = 1;
+                return 1;
+            }
+            //compute
+            if(depths[parent[index]] == 0)
+            {
+                computEfficientHeightHelper(parent, depths, parent[index]);
+            }
+            //depth is parent + 1
+            depths[index] = depths[parent[index]] + 1;
+            return depths[index];
+        }
+    }
 
-	static public void main(String[] args) throws IOException {
+	static public void main(String[] args) throws IOException 
+        {
             new Thread(null, new Runnable() {
-                
-                    public void run() {
-                        try {
-                            new tree_height().run();
-                        } catch (IOException e) {
-                        }
+                public void run() 
+                {
+                    try {
+                        new tree_height().run();
+                    } catch (IOException e) {
                     }
-                }, "1", 1 << 26).start();
+                }
+            }, "1", 1 << 26).start();
 	}
-	public void run() throws IOException {
-		TreeHeight tree = new TreeHeight();
-		tree.read();
-		System.out.println(tree.computeHeightOptimal());
+	public void run() throws IOException 
+        {
+            TreeHeight tree = new TreeHeight();
+            tree.read();
+            System.out.println(tree.computEfficientHeight());
 	}
 }
+
