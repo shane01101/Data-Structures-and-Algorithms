81dfa5778e9e5bc1d097ee721b1eaf6d886c55f5
Shane Honanie <shaneh425@gmail.com>
2018-07-19 13:57:04 -0700

    Binary Search

--- Algorithmic Toolbox/week4_divide_and_conquer/1_binary_search/BinarySearch.java
+++ Algorithmic Toolbox/week4_divide_and_conquer/1_binary_search/BinarySearch.java
@@ -3,11 +3,18 @@ import java.util.*;
 
 public class BinarySearch {
 
-    static int binarySearch(int[] a, int x) {
-        int left = 0, right = a.length;
-        //write your code here
-
-        return -1;
+    static int binarySearch(int[] a, int low, int high, int key) {
+        if (high < low)
+            return -1;
+        
+        int mid = low + ((high - low) /2);
+        
+        if(key == a[mid])
+            return mid;
+        else if (key < a[mid])
+            return binarySearch(a, low, mid - 1, key);
+        else
+            return binarySearch(a, mid + 1, high, key);
     }
 
     static int linearSearch(int[] a, int x) {
@@ -31,7 +38,8 @@ public class BinarySearch {
         }
         for (int i = 0; i < m; i++) {
             //replace with the call to binarySearch when implemented
-            System.out.print(linearSearch(a, b[i]) + " ");
+            System.out.print(binarySearch(a, 0, a.length-1, b[i]) + " ");
+//            System.out.print(linearSearch(a, b[i]) + " ");
         }
     }
     static class FastScanner {
