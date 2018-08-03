cc05cad992deda0206af92e9da66a06a4bc79ca5
Shane Honanie <shaneh425@gmail.com>
2018-08-02 22:09:35 -0700

    Dynammic Programming Calculator

--- Algorithmic Toolbox/Dynamic Programming/primitive_calculator/PrimitiveCalculator.java
+++ Algorithmic Toolbox/Dynamic Programming/primitive_calculator/PrimitiveCalculator.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
 public class PrimitiveCalculator {
-    private static List<Integer> optimal_sequence(int n) 
-    {
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+    
+    private static List<Integer> optimal_sequence(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
-        
-        while (n >= 1) 
-        {
+        while (n >= 1) {
             sequence.add(n);
             if (n % 3 == 0) {
                 n /= 3;
-            } else if (n % 2 == 0) 
-            {
+            } else if (n % 2 == 0) {
                 n /= 2;
             } else {
                 n -= 1;
@@ -21,45 +21,42 @@ public class PrimitiveCalculator {
         return sequence;
     }
     
-    private static List<Integer> efficient_sequence(int n) 
-    {
+    public static List<Integer> efficient_sequence(int n) {
         List<Integer> sequence = new ArrayList<Integer>();
-        int[] array = new int[n+1];
+        int[] arr = new int[n + 1];
         
-        for(int i = 1; i < n+1; i++)
-        {
-            array[i] = array[i-1] + 1;
+        //calculate counts for each number
+        for(int i = 1;i < n + 1; i++) {
+            arr[i] = arr[i - 1] + ONE;
             
-            if(i % 3 == 0)
-                array[i] = Math.min(array[i], array[i/3] + 1);
-            if(i % 2 == 0)
-                array[i] = Math.min(array[i], array[i/2] + 1);
+            if(i % THREE == 0)
+                arr[i] = Math.min(arr[i], arr[i/THREE] + 1);
+            if(i % TWO == 0)
+                arr[i] = Math.min(arr[i], arr[i/TWO] + 1);
+            
         }
         int k = n;
-        
-        while(k >= 1)
-        {
+        //backtrack and fill sequcnes of counts backwards
+        while (k >= 1) {
             sequence.add(k);
             
-            if(array[k] - 1 == array[k - 1])
-                k = k - 1;
-            else if(k % 2 == 0 && array[k] - 1 == array[k/2])
-                k = k / 2;
-            else if ((k % 3 == 0 && array[k] - 1 == array[k/3]))
-                k = k / 3;
+        if(arr[k - 1] == arr[k] - 1)
+            k--;
+        else if(k % TWO == 0 && arr[k / TWO] == arr[k] - 1)
+            k /= TWO;
+        else if(k % THREE == 0 && arr[k / THREE] == arr[k] - 1)
+            k /= THREE;
         }
         Collections.reverse(sequence);
         return sequence;
     }
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         List<Integer> sequence = efficient_sequence(n);
         System.out.println(sequence.size() - 1);
-        for (Integer x : sequence) 
-        {
+        for (Integer x : sequence) {
             System.out.print(x + " ");
         }
     }
--- Data Structures/Priority Queues and Disjoint Sets/job_queue/JobQueue.java
+++ Data Structures/Priority Queues and Disjoint Sets/job_queue/JobQueue.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class JobQueue {
@@ -9,9 +7,6 @@ public class JobQueue {
 
     private int[] assignedWorker;
     private long[] startTime;
-    
-    private List<Worker> workerPool;
-    int size;
 
     private FastScanner in;
     private PrintWriter out;
@@ -21,7 +16,7 @@ public class JobQueue {
     }
 
     private void readData() throws IOException {
-        numWorkers = size = in.nextInt();
+        numWorkers = in.nextInt();
         int m = in.nextInt();
         jobs = new int[m];
         for (int i = 0; i < m; ++i) {
@@ -34,8 +29,70 @@ public class JobQueue {
             out.println(assignedWorker[i] + " " + startTime[i]);
         }
     }
-
-    private void assignJobsNaive() {
+    private void assignJobsFast() {
+        Worker[] theHeap = buildHeap();
+        assignedWorker = new int[jobs.length];
+        startTime = new long[jobs.length];
+        
+        for(int i = 0; i < jobs.length; i++) {
+            assignedWorker[i] = getMin(theHeap);
+            increasePriority(theHeap, jobs[i]);
+            startTime[i] = i;
+        }
+    }
+    
+    public Worker[] buildHeap() {
+        Worker[] theHeap = new Worker[numWorkers];
+        
+        for(int i = theHeap.length/2; i >= 0; i--)
+            siftDown(theHeap, i);
+        
+        return theHeap;
+    }
+    
+    public void siftDown(Worker[] theHeap, int i) {
+        int size = numWorkers;
+        int minIndex = i;
+        int lChild = 2 * i + 1;
+        int rChild = 2 * i + 2;
+        
+        if(lChild < size && theHeap[minIndex].priority > theHeap[lChild].priority)
+            minIndex = lChild;
+        
+        if(rChild < size && theHeap[minIndex].priority > theHeap[rChild].priority)
+            minIndex = rChild;
+        
+        if(minIndex != i) {
+            Worker t = theHeap[minIndex];
+            theHeap[minIndex] = theHeap[i];
+            theHeap[i] = t;
+            siftDown(theHeap, minIndex);
+        }
+    }
+    
+//    public void siftUp(Worker[] theHeap, int i) {
+//        int parent = (i - 1) / 2;
+//        
+//        while(i > 0 && theHeap[i].priority < theHeap[parent].priority) {
+//            Worker t = theHeap[i];
+//            theHeap[i] = theHeap[parent];
+//            theHeap[parent] = t;
+//            
+//            i = parent;
+//        }
+//    }
+    
+    public int getMin(Worker[] theHeap) {
+        int result = theHeap[0].index;
+        return result;
+    }
+    
+    public void increasePriority(Worker[] theHeap, int time) {
+        theHeap[0].priority += time;
+        siftDown(theHeap, 0);
+    }
+    
+    private void assignJobsSlow() {
         // TODO: replace this code with a faster algorithm.
         assignedWorker = new int[jobs.length];
         startTime = new long[jobs.length];
@@ -52,76 +109,12 @@ public class JobQueue {
             nextFreeTime[bestWorker] += duration;
         }
     }
-    
-    private void assignJobsOptimal() {
-        assignedWorker = new int[jobs.length];
-        startTime = new long[jobs.length];
-        workerPool = new ArrayList<>();
-        
-        buildHeap();
-        for (int i = 0; i < jobs.length; i++) 
-        {
-            assignedWorker[i] = (int)workerPool.get(0).index;
-            startTime[i] = workerPool.get(0).priority;
-            increasePriority(0, jobs[i]);
-        }
-    }
-    
-    private void buildHeap() {
-      for(int i = 0; i < size ; i++)
-          workerPool.add(new Worker(i, 0));
-      
-      for(int i = size/2; i>=0; i--)
-          siftDown(i);
-    }
-    
-    private void siftDown(final int index)
-    {
-        int minIndex = index;
-        int leftChild = 2*index+1;
-        
-        if(leftChild < size)
-            if(workerPool.get(leftChild).priority < workerPool.get(minIndex).priority)
-                minIndex = leftChild;
-            else if(workerPool.get(leftChild).priority == workerPool.get(minIndex).priority)
-                if(workerPool.get(leftChild).index < workerPool.get(minIndex).index)
-                    minIndex = leftChild;
-        
-        int rightChild = 2*index+2;
-        
-        if(rightChild < size)
-            if(workerPool.get(rightChild).priority < workerPool.get(minIndex).priority)
-                minIndex = rightChild;
-            else if(workerPool.get(rightChild).priority == workerPool.get(minIndex).priority)
-                if(workerPool.get(rightChild).index < workerPool.get(minIndex).index)
-                    minIndex = rightChild;
-        
-        if(index != minIndex)
-        {
-            long temp = workerPool.get(index).priority;
-            workerPool.get(index).priority = workerPool.get(minIndex).priority;
-            workerPool.get(minIndex).priority = temp;
-            
-            long temp2 = workerPool.get(index).index;
-            workerPool.get(index).index = workerPool.get(minIndex).index;
-            workerPool.get(minIndex).index = temp2;
-            
-            siftDown(minIndex);
-        }
-    }
-    
-    private void increasePriority(final int index, final long newPriority)
-    {
-        long oldPriority = workerPool.get(index).priority;
-        workerPool.get(index).priority = newPriority + oldPriority;
-        siftDown(0);
-    }
 
     public void solve() throws IOException {
         in = new FastScanner();
         out = new PrintWriter(new BufferedOutputStream(System.out));
         readData();
-        assignJobsOptimal();
+        assignJobsSlow();
         writeResponse();
         out.close();
     }
@@ -147,13 +140,13 @@ public class JobQueue {
         }
     }
     
-    static class Worker {
-        long index;
-        long priority;
-
-        public Worker(long index, long priority) {
+    public class Worker {
+        private int index;
+        private int priority;
+        
+        public Worker(int index) {
             this.index = index;
-            this.priority = priority;
+            this.priority = index;
         }
     }
 }
