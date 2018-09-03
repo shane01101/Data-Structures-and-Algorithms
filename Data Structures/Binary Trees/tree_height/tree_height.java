2748f114f3ba47d5f103cf98e54c53c95509d061
Shane Honanie <shaneh425@gmail.com>
2018-09-03 16:07:01 -0700

    Tree Height Solution

--- Data Structures/Binary Trees/tree_height/tree_height.java
+++ Data Structures/Binary Trees/tree_height/tree_height.java
@@ -1,52 +1,42 @@
 import java.util.*;
 import java.io.*;
 
-public class tree_height 
-{
-    class FastScanner 
-    {
+public class tree_height {
+    class FastScanner {
         StringTokenizer tok = new StringTokenizer("");
         BufferedReader in;
 
-        FastScanner() 
-        {
+        FastScanner() {
             in = new BufferedReader(new InputStreamReader(System.in));
         }
 
-        String next() throws IOException 
-        {
+        String next() throws IOException {
             while (!tok.hasMoreElements())
                 tok = new StringTokenizer(in.readLine());
             return tok.nextToken();
         }
-        int nextInt() throws IOException 
-        {
+        int nextInt() throws IOException {
             return Integer.parseInt(next());
         }
     }
 
-    public class TreeHeight 
-    {
+    public class TreeHeight {
         int n;
         int parent[];
 
-        void read() throws IOException 
-        {
+        void read() throws IOException {
             FastScanner in = new FastScanner();
             n = in.nextInt();
             parent = new int[n];
-            for (int i = 0; i < n; i++) 
-            {
-                parent[i] = in.nextInt();
+            for (int i = 0; i < n; i++) {
+                    parent[i] = in.nextInt();
             }
         }
 
-        int computeHeight() 
-        {
+        int computeHeightSlow() {
             // Replace this code with a faster implementation
             int maxHeight = 0;
-            for (int vertex = 0; vertex < n; vertex++) 
-            {
+            for (int vertex = 0; vertex < n; vertex++) {
                 int height = 0;
                 for (int i = vertex; i != -1; i = parent[i])
                     height++;
@@ -55,61 +45,67 @@ public class tree_height
             return maxHeight;
         }
         
-        int computEfficientHeight() 
-        {
-            int[] nodeDepth = new int[n];
-            int height = 0;
+        int buildTreeAndGetHeight() {
+            Node[] tree = new Node[n];
+            int root = -1;
             
-            if(n==1 && parent[0] == -1)
-                return 1;
+            for(int i = 0; i < n; i++)
+                tree[i] = new Node();
             
-            for(int i = 0; i < nodeDepth.length; i++)
-            {
-                height = Math.max(height, computEfficientHeightHelper(parent, nodeDepth, i));
+            for(int i = 0; i < n; i++) {
+               if(parent[i] == -1)
+                   root = i;
+               else {
+                   tree[parent[i]].addChild(i);
+               }
             }
-                
-            return height;
+            return computeHeightFast(tree, root);
         }
         
-        int computEfficientHeightHelper(int[] parent, int[] depths, int index)
-        {
-            //already computed
-            if(depths[index] > 0)
-                return depths[index];
-            //root
-            if(parent[index] == -1)
-            {
-                depths[index] = 1;
+        int computeHeightFast(Node[] tree, int cur) {
+            if(tree[cur].isEmptyChildren())
                 return 1;
+            
+            List<Integer> heights = new ArrayList<>();
+            
+            for(int x: tree[cur].children) {
+                heights.add(computeHeightFast(tree, x));
             }
-            //compute
-            if(depths[parent[index]] == 0)
-            {
-                computEfficientHeightHelper(parent, depths, parent[index]);
-            }
-            //depth is parent + 1
-            depths[index] = depths[parent[index]] + 1;
-            return depths[index];
+            Collections.sort(heights);
+            
+            return 1 + heights.get(heights.size() - 1);
+        }
+    }
+    
+    public class Node {
+        List<Integer> children;
+        public Node() {
+            children = new ArrayList<>();
+        }
+        
+        public void addChild(int x) {
+            children.add(x);
+        }
+        
+        public boolean isEmptyChildren() {
+            return children.isEmpty();
         }
     }
 
-	static public void main(String[] args) throws IOException 
-        {
-            new Thread(null, new Runnable() {
-                public void run() 
-                {
-                    try {
-                        new tree_height().run();
-                    } catch (IOException e) {
-                    }
-                }
-            }, "1", 1 << 26).start();
-	}
-	public void run() throws IOException 
-        {
-            TreeHeight tree = new TreeHeight();
-            tree.read();
-            System.out.println(tree.computEfficientHeight());
-	}
+    static public void main(String[] args) throws IOException {
+        new Thread(null, new Runnable() {
+            public void run() {
+                try {
+                    new tree_height().run();
+                } catch (IOException e) {}
+            }
+        }, "1", 1 << 26).start();
+    }
+    public void run() throws IOException {
+        TreeHeight tree = new TreeHeight();
+        tree.read();
+        ///tree.computeHeightFast();
+        System.out.println(tree.buildTreeAndGetHeight());
+        //System.out.println(tree.computeHeightSlow());
+    }
 }
-
